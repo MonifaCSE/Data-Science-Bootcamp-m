95f00105a990f7bae4cb2ec55f36005901af53e8
Monifa <monifasultana5637@gmail.com>
2022-09-06 18:10:57 +0600

    2

---
+++ Week1/Hacker rank/Arithmatic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a = int(input())
+b = int(input())
+
+s=a+b
+d=a-b
+p=a*b
+
+print(f"{s}\n{d}\n{p}")
+
+
+# In[ ]:
+
+
+
+
---
+++ Week1/Hacker rank/ifelse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+n = int(input())
+
+if (n%2!=0) or (n%2==0 and n>=6 and n<=20):
+    print("Weird")
+else:
+    print("Not Weird")
+
+
+# In[ ]:
+
+
+
+
